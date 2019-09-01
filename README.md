4070fb9af0b3de646c325f7a8656ec59162c83db
Leon <89leon@gmail.com>
2019-09-01 16:42:34 +0300

    structure change + docker-compose added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# gan_shmuel_green
+#LOCAL RUN INSTRUCTIONS 
+docker build -t providers_mysql .
+docker run -it -d -e MYSQL_ROOT_PASSWORD=123  providers_mysql
+docker exec -it <container_name> bash
+#INSIDE CONTAINER
+mysql -u root -p                      // pass = 123 
+
+#DB COMMANDS
+show databases
+use <db_name>
+show tables
+.
+.
--- provider.py
+++ provider.py
@@ -1,16 +1,11 @@
 from flask import Flask , render_template , redirect , request , jsonify , json , Response
 app = Flask(__name__, template_folder='.')
 import os
+import mysql.connector
 from flaskext.mysql import MySQL
 
 
-app.config['MYSQL_HOST'] = '0.0.0.0'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123'
-app.config['MYSQL_DB'] = 'billdb'
 
-
-mysql = MySQL(app)
 
 
 
@@ -22,7 +17,8 @@ def index():
 
 @app.route('/health')
 def health():
-	return '{ "ErrorCode" : 0 , "Description" : "OK" }'	
+
+    return '{ "ErrorCode" : 0 , "Description" : "OK" }'	
   
 app.run(host='0.0.0.0')
 
