32f1f85cdffec3d9cc03d795c6cd633adb6ffac8
Leon <89leon@gmail.com>
2019-09-01 14:43:37 +0300

    edit docker file in order to allow db access

--- flask/provider.py
+++ flask/provider.py
@@ -1,6 +1,18 @@
 from flask import Flask , render_template , redirect , request , jsonify , json , Response
 app = Flask(__name__, template_folder='.')
 import os
+from flaskext.mysql import MySQL
+
+
+app.config['MYSQL_HOST'] = '0.0.0.0'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123'
+app.config['MYSQL_DB'] = 'billdb'
+
+
+mysql = MySQL(app)
+
+
 
 @app.route('/')
 @app.route('/index')
