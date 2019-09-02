a3e50983b0c0ed62c97b31ed9f09b9142182d056
Kobi Avshalom <kobiavshalom@gmail.com>
2019-09-02 15:02:45 +0300

    fix of auto prod

--- Devops/productionEnv/startProd.py
+++ Devops/productionEnv/startProd.py
@@ -14,18 +14,15 @@ def runEnv():
 os.chdir('/home/ubuntu/prod')
 deleteRepo = 'rm -rf gan_shmuel_green/'
 gitcloneCMD = 'git clone https://github.com/greendeveleap/gan_shmuel_green.git'
-answerPW = '12345'
 rmDCfile = 'rm docker-compose.yml'
 copyGlobalDCF = 'cp /home/ubuntu/src/dockerComposeFiles/dcFIles/PP/docker-compose.yml /home/ubuntu/prod/gan_shmuel_green/providers/docker-compose.yml'
+copyGlobalDCFW = 'cp /home/ubuntu/src/dockerComposeFiles/dcFIles/WP/docker-compose.yml /home/ubuntu/prod/gan_shmuel_green/weight/docker-compose.yml'
 os.system(deleteRepo)
 print("Repo deleted from prod dir")
 sleep(3)
 os.system(gitcloneCMD)
 print("git clone cmd executed")
 sleep(5)
-#os.system(answerPW)
-#print("password set")
-#sleep(2)
 os.chdir('/home/ubuntu/prod/gan_shmuel_green/providers')
 sleep(2)
 os.system(rmDCfile)
@@ -33,6 +30,14 @@ print("DC file removed from repo")
 sleep(2)
 os.system(copyGlobalDCF)
 print ("New DC file added to the repo")
+sleep(2)
+runEnv()
+sleep(3)
+os.chdir('/home/ubuntu/prod/gan_shmuel_green/weight')
+sleep(2)
+os.system(rmDCfile)
+sleep(2)
+os.system(copyGlobalDCFW)
 sleep(2)
 runEnv()
 
