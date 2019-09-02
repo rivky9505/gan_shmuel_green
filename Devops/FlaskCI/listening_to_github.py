3eef35c25ad4db2c2595b6d0f77c48ca55f6df84
Kobi Avshalom <kobiavshalom@gmail.com>
2019-09-02 12:29:40 +0300

    fixing test

--- Devops/FlaskCI/listening_to_github.py
+++ Devops/FlaskCI/listening_to_github.py
@@ -25,7 +25,8 @@ def api():
 def api_gh_message():
     if request.headers['Content-Type'] == 'application/json':
         info = json.dumps(request.json)
-        branch = info[info.find("refs/heads")+10:info.find("refs/heads")+50]   
+        branch = info[info.find("refs/heads")+10:info.find("refs/heads")+50]
+        branch = branch[1:branch.find("", "")]
         print (info)
         print (branch)
         return info
