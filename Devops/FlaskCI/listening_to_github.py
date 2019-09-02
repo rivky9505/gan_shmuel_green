67c355809f1679df3ca9c5667131ee0c84d1d0b7
Kobi Avshalom <kobiavshalom@gmail.com>
2019-09-02 12:18:14 +0300

    fixing listner

--- Devops/FlaskCI/listening_to_github.py
+++ Devops/FlaskCI/listening_to_github.py
@@ -2,12 +2,30 @@ from flask import json, request, Flask, debughelpers
 
 app = Flask(__name__)
 
+def find_str(s, char):
+    index = 0
+
+    if char in s:
+        c = char[0]
+        for ch in s:
+            if ch == c:
+                if s[index:index+len(char)] == char:
+                    return index
+
+            index += 1
+
+    return -1
+
+@app.route('/')
+def api():
+    strd = "Happy birthday"
+    return str(strd.find("py"))
+
 @app.route('/github', methods=['POST'])
 def api_gh_message():
     if request.headers['Content-Type'] == 'application/json':
         info = json.dumps(request.json)
-        
-        branch = info.ref
+        branch = info[info.find("ref"):info.find("ref")+10]   
         print (info)
         print (branch)
         return info
