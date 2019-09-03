9d1708588ba9f578ea81c0dfdbcdc8c2709198bc
ofir <ofiramit3@gmail.com>
2019-09-03 14:35:35 +0300

    changed test.sql

--- weight/test.sql
+++ weight/test.sql
@@ -13,18 +13,6 @@ CREATE DATABASE IF NOT EXISTS `weight`;
  
  
 USE weight;
-<<<<<<< HEAD
-
-=======
- 
-CREATE TABLE IF NOT EXISTS `unknown` (
-  `container_id` varchar(15) NOT NULL,
-  `weight` int(12) DEFAULT NULL,
-  `unit` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`container_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10001 ;
- 
->>>>>>> eb34e2161c1de781e0f76f4d9502c36b8c1d4444
  
 CREATE TABLE IF NOT EXISTS `containers_registered` (
   `container_id` varchar(15) NOT NULL,
@@ -85,13 +73,7 @@ CREATE TABLE IF NOT EXISTS `weight` (
 
 INSERT INTO weight (`direction`, `truckid`, `containers`, `bruto`, `unit`, `forc`, `produce`) VALUES ('in', 'truckID', 'str10', '10', 'unit', '0', 'tomatoes');
 
-<<<<<<< HEAD
-
-INSERT INTO Trucks (`id`, `provider_id`) VALUES ('134-33-443', 10001), ('124-55-443', 10003),
-('222-33-111', 10003), ('212-33-441', 10004),('432-98-541', 10001), ('212-99-466', 10002);
-=======
 ----Table structure for sessions
->>>>>>> eb34e2161c1de781e0f76f4d9502c36b8c1d4444
 
 
 --GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
