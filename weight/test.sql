2b5f1ded7332a063d30190d2522f06059d38cecc
Develeap <dakar.tal@develeap.com>
2019-09-03 16:52:43 +0300

    Fixed SQL

--- weight/test.sql
+++ weight/test.sql
@@ -1,34 +1,31 @@
 --
 -- Database: `Weight`
 --
- 
+
 CREATE DATABASE IF NOT EXISTS `weight`;
- 
- 
+
 -- --------------------------------------------------------
- 
+
 --
 -- Table structure for table `containers-registered`
 --
- 
- 
+
 USE weight;
- 
+
+
 CREATE TABLE IF NOT EXISTS `containers_registered` (
   `container_id` varchar(15) NOT NULL,
-  `weight` varchar(12) DEFAULT NULL,
-  `unit` varchar(12) DEFAULT NULL,
+  `weight` int(12) DEFAULT NULL,
+  `unit` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`container_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=10001 ;
- 
-
 
 -- --------------------------------------------------------
- 
+
 --
 -- Table structure for table `transactions`
 --
- 
+
 CREATE TABLE IF NOT EXISTS `transactions` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `datetime` datetime DEFAULT NULL,
@@ -41,21 +38,17 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `neto` int(12) DEFAULT NULL,
   `produce` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-
 ) ENGINE=MyISAM AUTO_INCREMENT=10001 ;
- 
-
-INSERT INTO transactions(`id`, `datetime`, `direction`) VALUES ('10', '2011-12-18 13:17:17', 'in');
 
 show tables;
- 
+
 describe containers_registered;
 describe transactions;
-describe unknown;
- 
- 
+
+
 -- Table structure for table 'weight'
- 
+
+
 CREATE TABLE IF NOT EXISTS `weight` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -70,23 +63,27 @@ CREATE TABLE IF NOT EXISTS `weight` (
 
 
 
-
-INSERT INTO weight (`direction`, `truckid`, `containers`, `bruto`, `unit`, `forc`, `produce`) VALUES ('in', 'truckID', 'str10', '10', 'unit', '0', 'tomatoes');
-
-----Table structure for sessions
-
-
---GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
---FLUSH PRIVILEGES;
+INSERT INTO weight (`direction`, `truckid`, `containers`, `bruto`, `unit`, `forc`, `produce`) VALUES ('in', 'TRUCKID', 'containers', '10', 'kg', '0', 'tomatoes');
 
 
 
 
-CREATE TABLE IF NOT EXISTS 'sessions' (
-  `id` int(12) NOT NULL AUTO_INCREMENT, 
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  `truckid` varchar(50) DEFAULT NULL, 
-  `bruto` int(12) DEFAULT NULL, 
-  `truckTara` int(12) DEFAULT NULL, 
-  `neto` int(12) DEFAULT NULL, 
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `truckid` varchar(50) DEFAULT NULL,
+  `bruto` int(12) DEFAULT NULL,
+  `truckTara` int(12) DEFAULT NULL,
+  `neto` int(12) DEFAULT NULL,
 PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=10001
+
+
+
+
+--
+-- Dumping data for table `test`
+--
+
+-- INSERT INTO `test` (`id`, `aa`) VALUES
+-- (1, 'aaaa'),
+                                                      
