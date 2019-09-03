5b248a56259835a51d66b0546b61f8f980760ebe
Saar <hire.saar@gmail.com>
2019-09-03 13:02:04 +0300

    "SQL minor fix"

--- weight/test.sql
+++ weight/test.sql
@@ -83,98 +83,11 @@ INSERT INTO weight (`direction`, `truckid`, `containers`, `bruto`, `unit`, `forc
 ----Table structure for sessions
 
 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+--GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+--FLUSH PRIVILEGES;
 
 
 
-CREATE TABLE IF NOT EXISTS `Provider` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  AUTO_INCREMENT=10001 ;
- 
-CREATE TABLE IF NOT EXISTS `Rates` (
-  `product_id` varchar(50) NOT NULL,
-  `rate` int(11) DEFAULT 0,
-  `scope` varchar(50) DEFAULT NULL,
-  FOREIGN KEY (scope) REFERENCES `Provider`(`id`)
-) ENGINE=MyISAM ;
- 
-CREATE TABLE IF NOT EXISTS `Trucks` (
-  `id` varchar(10) NOT NULL,
-  `provider_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`provider_id`) REFERENCES `Provider`(`id`)
-) ENGINE=MyISAM ;
---
--- Dumping data
---
- 
-
-INSERT INTO Provider (`name`) VALUES ('ALL'), ('provider 1'), ('provider 2'), ('provider 3'), ('provider 4');
- 
-INSERT INTO Rates (`product_id`, `rate`, `scope`) VALUES ('Navel', '93', 'All'), ('Blood', '112', 'All'), ('Mandarin', '104', 'All'), 
-('Shamuti', '84', 'All'), ('Tangerine', '92', 'All'), ('Clementine', '113', 'All'), 
-('Grapefruit', '88', 'All'), ('Valencia', '87', 'All'), ('Mandarin', '102', '43'), 
-('Mandarin', '120', '45'), ('Tangerine', '85', '12'), ('Valencia', '90', '45');
- 
-INSERT INTO unknown (container_id , weight ,unit) VALUES ('1c' , '400' , 'NULL');
-INSERT INTO unknown (container_id , weight ,unit) VALUES ('1d' , '800' , 'NULL');
-
- 
-
-
---
--- Dumping data for table `test`
---
- 
--- INSERT INTO `test` (`id`, `aa`) VALUES
--- (1, 'aaaa'),
-
-CREATE TABLE IF NOT EXISTS `Provider` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  AUTO_INCREMENT=10001 ;
- 
-CREATE TABLE IF NOT EXISTS `Rates` (
-  `product_id` varchar(50) NOT NULL,
-  `rate` int(11) DEFAULT 0,
-  `scope` varchar(50) DEFAULT NULL,
-  FOREIGN KEY (scope) REFERENCES `Provider`(`id`)
-) ENGINE=MyISAM ;
- 
-CREATE TABLE IF NOT EXISTS `Trucks` (
-  `id` varchar(10) NOT NULL,
-  `provider_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`provider_id`) REFERENCES `Provider`(`id`)
-) ENGINE=MyISAM ;
---
--- Dumping data
---
- 
-
-INSERT INTO Provider (`name`) VALUES ('ALL'), ('provider 1'), ('provider 2'), ('provider 3'), ('provider 4');
- 
-INSERT INTO Rates (`product_id`, `rate`, `scope`) VALUES ('Navel', '93', 'All'), ('Blood', '112', 'All'), ('Mandarin', '104', 'All'), 
-('Shamuti', '84', 'All'), ('Tangerine', '92', 'All'), ('Clementine', '113', 'All'), 
-('Grapefruit', '88', 'All'), ('Valencia', '87', 'All'), ('Mandarin', '102', '43'), 
-('Mandarin', '120', '45'), ('Tangerine', '85', '12'), ('Valencia', '90', '45');
- 
-INSERT INTO unknown (container_id , weight ,unit) VALUES ('1c' , '400' , 'NULL');
-INSERT INTO unknown (container_id , weight ,unit) VALUES ('1d' , '800' , 'NULL');
-
- 
-
-
---
--- Dumping data for table `test`
---
- 
--- INSERT INTO `test` (`id`, `aa`) VALUES
--- (1, 'aaaa'),
 
 CREATE TABLE IF NOT EXISTS 'sessions' (
   `id` int(12) NOT NULL AUTO_INCREMENT, 
