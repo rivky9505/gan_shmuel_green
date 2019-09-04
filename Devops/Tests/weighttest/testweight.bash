79256f73a086d6583aad6b6b6bb2744d3e3723b8
dan <dan@gmail.com>
2019-09-04 09:36:16 +0300

    reorgnise branch tests.

--- Devops/Tests/weighttest/testweight.bash
+++ Devops/Tests/weighttest/testweight.bash
@@ -5,15 +5,21 @@ git clone git@github.com:greendeveleap/gan_shmuel_green.git
 cd gan_shmuel_green
 git checkout weight
 cd ..
-touch "$(pwd)"/endtoend/logs/end2endreport.log
-docker-compose up --build -d
-sleep 30
-python3 "$(pwd)"/endtoend/endtoendWeight.py
-sleep 5
-docker-compose down
-status=$(python3 testlog.py)
-echo status = $status
 
+#create logfile
+touch "$(pwd)"/endtoend/logs/end2endreport.log
+
+#dockerize
+docker-compose up --build -d
+sleep 30 #wait for docker to fully go up
+python3 "$(pwd)"/endtoend/endtoendWeight.py
+sleep 3 
+docker-compose down
+
+#checklog
+status=$(python3 testlog.py)
+
+#print 0 if all tests pass else 1
 if [ $status -eq 0 ]
 then
     echo 0
@@ -21,8 +27,7 @@ else
     echo 1
 fi
 
-
+#remove log+repo+docker images
 rm -rf gan_shmuel_green
 rm "$(pwd)"/endtoend/logs/end2endreport.log
 docker rmi $(docker images -q) --force
-
