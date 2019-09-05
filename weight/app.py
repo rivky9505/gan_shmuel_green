1c9939f5e9f9002ce77b617e8a572a6ad452d785
Develeap <dakar.tal@develeap.com>
2019-09-05 11:42:58 +0300

    Latest

--- weight/app.py
+++ weight/app.py
@@ -7,8 +7,6 @@ import logging, datetime , sys
 import csv
 app = Flask(__name__)
 
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
 def getMysqlConnection():
     return mysql.connector.connect(user='root', host='mysql', port='3306', password='root', database='weight')
 
@@ -42,17 +40,20 @@ def get_unknown():
     cur = db.cursor()
     try:
         data_query = "SELECT * FROM containers_registered"
+        # logging.info("Looking for items with unknown weights")
+        cur = db.cursor()
         cur.execute(data_query)
         output_json = cur.fetchall()
         unknowns = []
         for row in output_json :
             if ((row[1]) == 'na') :
                 unknowns.append(row[0])
+
     except Exception as e:
-        logging.error('[GET][FAILURE] /health request . QUERY:' + data_query)
+        logging.error("ERROR , while trying : %s", data_query)
         return jsonify("500 Internal server error")
     finally:
-        logging.info("[GET][SUCCESS] get item  request - : %s", (data_query))
+        logging.info("200 OK Weight is healthy")
         db.close()
     return render_template('unknown.html' ,unknowns=unknowns)
 
@@ -157,7 +158,7 @@ def get_item_id(id_num):
                 return jsonify("404 no id found")
             truck = 1
     except Exception:
-        logging.error('[GET][FAILURE] /health request . QUERY:' + id_query)
+        logging.error("ERROR , while trying : get item")
         return jsonify("nope")
 
     try:
@@ -170,13 +171,12 @@ def get_item_id(id_num):
                 last_weight = line[4]
                 ret_id.append(line[0])
             json_data = {'id': id_num , 'tara': last_weight , 'sessions' :ret_id }
-            logging.info("[GET][SUCCESS] get item  request - : %s", (data_query))
             return jsonify(json_data)
         else:
             logging.error("no session found")
             return jsonify("no session found")
     except Exception:
-        logging.error('[GET][FAILURE] /health request . QUERY:' + data_query)
+        logging.error("ERROR , while trying : get item")
         return jsonify("404 item id not found")
     finally:
         logging.info("200 OK Weight is healthy")
@@ -231,23 +231,6 @@ def postweight():
         forc = details['forc']
         truckTara = details['bruto']
         produce = details['produce']
-        direction = request.args.get('direction')
-        containers = request.args.get('containers')
-        truckid = request.args.get('truckid')
-        bruto = request.args.get('bruto')
-        unit = request.args.get('unit')
-        forc = request.args.get('forc')
-        truckTara = request.args.get('bruto')
-        produce = request.args.get('produce')
-        direction = request.args.get('direction')
-        containers = request.args.get('containers')
-        truckid = request.args.get('truckid')
-        bruto = request.args.get('bruto')
-        unit = request.args.get('unit')
-        forc = request.args.get('forc')
-        truckTara = request.args.get('bruto')
-        produce = request.args.get('produce')
-        return "1"
 
         cur = db.cursor()
         cur.execute("INSERT INTO weight(direction, truckid, containers, bruto, unit, forc) VALUES (%s, %s, %s, %s, %s, %s)", (direction, truckid, containers, bruto, unit, forc))
@@ -265,7 +248,7 @@ def postweight():
         output_json2 = cur.fetchall()
         b = output_json2
         pformat_b = pprint.pformat(b)
-
+        
         strip_checkin = ""
 
         if pformat_a == pformat_b and strip_checkin == strip_true:
@@ -279,7 +262,7 @@ def postweight():
 
             cur.execute("INSERT INTO sessions(direction, truckid, bruto) VALUES (%s, %s, %s)", (direction, truckid, bruto))
 
-        
+                
         ischeckin = "SELECT direction='in' from weight where truckid='%s' LIMIT 1;"%strip_truckid
         ischeckout = 'SELECT truckid from weight;'
         checkout_session = "SELECT id from sessions where truckid='%s' ORDER BY created_at DESC LIMIT 1;"%strip_truckid
@@ -331,7 +314,7 @@ def postweight():
         strip_truckTara = strip(pformat_truckTara)
 
 #        strip_bruto = int(strip_bruto)
-      
+
 #        strip_truckTara = int(strip_truckTara)
  #       neto = (strip_bruto - strip_truckTara)
       
@@ -369,109 +352,14 @@ def postweight():
 
 @app.route('/session/<id>', methods=['GET'])
 def session(id):
-
-
-
-# GET /weight?from=t1&to=t2&filter=f
-# - t1,t2 - date-time stamps, formatted as yyyymmddhhmmss. server time is assumed.
-# - f - comma delimited list of directions. default is "in,out,none"
-# default t1 is "today at 000000". default t2 is "now". 
-# returns an array of json objects, one per weighing (batch NOT included):
-# [{ "id": <id>,
-#    "direction": in/out/none,
-#    "bruto": <int>, //in kg
-#    "neto": <int> or "na" // na if some of containers have unknown tara
-#    "produce": <str>,
-#    "containers": [ id1, id2, ...]
-# },...]
-
-@app.route('/weight', methods=['GET'])
-def getweight():
-    if request.method == "GET":
-            db = getMysqlConnection()
-            serverTime = datetime.datetime.now().strftime("%Y%m%d%I%M%S")
-            t1 = request.form.get('from', default = serverTime , type = str)
-            t2 = request.form.get('to', default = "now" , type = str)
-            filter = request.form.get('filter', default = "in,out,none" , type = str)
-            filter = str(filter).split(',')
-        # data_query = ("SELECT * FROM  sessions WHERE created_at>="  + from_t1 +" AND created_at<=" + to_t2 + " AND truckid=" + "'" +id_num +"'" + "ORDER BY created_at ASC")
-
-            lines = "select * from weight where " + "created_at>='" + t1 + "' and created_at<='" + t2 + "'"
-            cur = db.cursor()
-            cur.execute(lines)
-            output_transactions = cur.fetchall()
-
-            weightsList = []
-
-            for line in output_transactions :
-                if line[2] in filter :
-                    if any(item in str(line[4]).split(',')  for item in get_unknown()):
-                        neto = None
-                    else:
-                        neto = line[7]
-
-                    retweight = { 'id': line[0], 'truck': line[3],'direction': line[2], 'bruto': line[5], 'neto': neto, 'produce': line[8], 'containers': str(line[4]).split(',') }
-                    weightsList.append(retweight)
-    return jsonify({'weights': weightsList})                    
-
-
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
-
-@app.route('/session/<id>', methods=['GET'])
-def session(id):
-
-
-
-# GET /weight?from=t1&to=t2&filter=f
-# - t1,t2 - date-time stamps, formatted as yyyymmddhhmmss. server time is assumed.
-# - f - comma delimited list of directions. default is "in,out,none"
-# default t1 is "today at 000000". default t2 is "now". 
-# returns an array of json objects, one per weighing (batch NOT included):
-# [{ "id": <id>,
-#    "direction": in/out/none,
-#    "bruto": <int>, //in kg
-#    "neto": <int> or "na" // na if some of containers have unknown tara
-#    "produce": <str>,
-#    "containers": [ id1, id2, ...]
-# },...]
-
-@app.route('/weight', methods=['GET'])
-def getweight():
-    if request.method == "GET":
-            db = getMysqlConnection()
-            serverTime = datetime.datetime.now().strftime("%Y%m%d%I%M%S")
-            t1 = request.form.get('from', default = serverTime , type = str)
-            t2 = request.form.get('to', default = "now" , type = str)
-            filter = request.form.get('filter', default = "in,out,none" , type = str)
-            filter = str(filter).split(',')
-        # data_query = ("SELECT * FROM  sessions WHERE created_at>="  + from_t1 +" AND created_at<=" + to_t2 + " AND truckid=" + "'" +id_num +"'" + "ORDER BY created_at ASC")
-
-            lines = "select * from weight where " + "created_at>='" + t1 + "' and created_at<='" + t2 + "'"
-            cur = db.cursor()
-            cur.execute(lines)
-            output_transactions = cur.fetchall()
-
-            weightsList = []
-
-            for line in output_transactions :
-                if line[2] in filter :
-                    if any(item in str(line[4]).split(',')  for item in get_unknown()):
-                        neto = None
-                    else:
-                        neto = line[7]
-
-                    retweight = { 'id': line[0], 'truck': line[3],'direction': line[2], 'bruto': line[5], 'neto': neto, 'produce': line[8], 'containers': str(line[4]).split(',') }
-                    weightsList.append(retweight)
-    return jsonify({'weights': weightsList})                    
-
-
-@app.route('/session/<string:session_id>', methods=['GET'])
-def session(session_id):
     db = getMysqlConnection()
+    if request.method == 'GET':
+
+        getsession = "SELECT JSON_ARRAYAGG(JSON_OBJECT('id', id, 'created', created_at, 'truckid', truckid, 'Bruto', bruto, 'truckTara', truckTara, 'Neto', neto)) from sessions where id='%s'" %id
+
+
         cur = db.cursor()
-    id_query = ("SELECT * FROM  sessions WHERE id=" + "'" + session_id + "'")
-    cur.execute(id_query)
+        cur.execute(getsession)
         output_session = cur.fetchall()
 
         return jsonify(output_session)
@@ -485,9 +373,3 @@ def session(session_id):
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
 
-    return jsonify(results=output_session)
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
